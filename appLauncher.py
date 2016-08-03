47109d50caf5a5a960fc1d5a2b679551b3e952a3
u0823377 <u0823377@staff-checkout-mac-laptop-1.net.utah.edu>
2016-08-03 17:19:54 -0600

    some ui improvements, shadow path added but broken

--- appLauncher.py
+++ appLauncher.py
@@ -1,15 +1,15 @@
 # Author: Jaxon Brewer
-# 
+# Date: August 3, 2016
+#
 # Created for the Marriot Library at the University of Utah
 #
 # This script is used to wrap applications that behave poorly
-# in a locked down evironment.It launches an application in a
+# in a locked down evironment. It launches an application in a
 # sandbox environment that has not effect on the host system
 # of the application.
 
 import os
 import sys
-import getpass
 import plistlib
 from subprocess import call
 
@@ -20,36 +20,27 @@ def launch():
 	diskName = appName
 	appPath = mountRoot + '/' + diskName + '/' + appName + '.app'
 	imagePath = os.path.realpath(appName + '.dmg')
-	shadowPath = '/tmp/' + appName + '.shadow'
-	
-	currentUser = getpass.getuser()
-	reownScript = '/usr/local/bin/' + appName.lower() + '_change_owner.sh'
+	shadowPath = appInfo['ShadowPath']
 
 	# Check that the app image is mounted, mount it if it is not
 	if not os.path.ismount(mountRoot + '/' + diskName):
 		''' Mount the image at imagePath with:
-		 * -nobrowse		Prevents the volume from appearing in 
+		 * -nobrowse		Prevents the volume from appearing in
 		                   Finder or on the Desktop.
-		 * -noautoopenro	Makes sure we won't accidentally pop 
+		 * -noautoopenro	Makes sure we won't accidentally pop
                            open a Finder window into the volume.
-		 * -noverify		Ensures that hdiutil will not verify 
+		 * -noverify		Ensures that hdiutil will not verify
                            the volume. Speeds up mount times.
 		 * -mountroot		Changes where the volume is mounted to.
-		 * -shadow			Allows read/write access by creating 
-                           a file the user can edit and shadowing 
+		 * -shadow			Allows read/write access by creating
+                           a file the user can edit and shadowing
                            what would have been the edits there.'''
 		call(['hdiutil', 'attach', '-nobrowse', '-owners', 'on', '-noautoopenro', '-noverify', '-mountroot',  mountRoot, '-shadow',  shadowPath, imagePath])
 
-		#call(['sudo', reownScript, currentUser])
-	
+
 	# Launch application for user
 	if os.path.exists(appPath):
-		# * -W	Causes open to block until app exits
-		# * -n	opens a new instance of the app
 		call(['/usr/bin/open', '-a', appPath])
 
 
 launch()
-
-
-
--- app_creator_ui.py
+++ app_creator_ui.py
@@ -46,6 +46,7 @@ class Application(Frame):
 		self.iconEntry.insert(0, appIconName)
 
 		'''
+		# This is code for displaying the app's icon if PIL is not broken
 		iconPath = self.src + '/Contents/Resources/' + appIconName
 		if not iconPath.endswith('.icns'):
 			iconPath += '.icns'
@@ -60,29 +61,41 @@ class Application(Frame):
 		return True
 
 	def openFile(self):
+		# Clear the input and then get a file from the dialog
 		self.srcEntry.delete(0, END)
 		self.srcEntry.insert(0, tkFileDialog.askopenfilename(**self.file_opt))
 		self.getinfo()
 
 	def openDir(self):
+		# Clear the input and get the destination from the dialog
 		self.destEntry.delete(0, END)
 		self.destEntry.insert(0, tkFileDialog.askdirectory(**self.dir_opt))
 
+	# returns false if app creation fails
 	def createApp(self):
 		# Setup variables
 		APP = ['appLauncher.py']
 		DATA_FILES = []
 		OPTIONS = {'argv_emulation': True}
 
+		appName = self.nameEntry.get()
+		shadowPath = self.shadowEntry.get()
+		if not shadowPath:
+			shadowPath = '/tmp/' + appName + '.shadow'
+		else:
+			shadowPath = os.path.join(shadowPath, appName + '.shadow')
+
 		# Custom information for info.plist
-		infoPlist = {'CFBundleShortVersionString': self.longVersionEntry.get(),
-					 'CFBundleIdentifier': 'edu.utah.scl.' + self.nameEntry.get().lower() + 'wrapper'}
+		infoPlist = {'CFBundleShortVersionString': self.shortVersionEntry.get(),
+					 'CFBundleVersion': self.longVersionEntry.get(),
+					 'CFBundleIdentifier': 'edu.utah.scl.' + appName.lower() + 'wrapper',
+					 'ShadowPath': shadowPath}
 		OPTIONS['plist'] = infoPlist
 
 		dest = self.destEntry.get()
 		if not os.path.exists(dest):
 			# destination Invalid
-			exit()
+			return False
 		OPTIONS['dist_dir'] = dest
 
 		# Create path for icon and append .icns if not already there
@@ -98,75 +111,81 @@ class Application(Frame):
 
 		# Create app image
 		#TODO: create option for setting max image size
-		imagePath = tempDir + '/' + self.nameEntry.get() + '.dmg'
+		imagePath = tempDir + '/' + appName + '.dmg'
 		call(['hdiutil', 'create', '-srcfolder',  self.src, imagePath])
 
 		DATA_FILES.append(imagePath)
 
-		# py2app requires special arguments
+		# py2app requires special arguments, the semi-standalone flag is being used
+		# because it crashes without, however this makes the app rely on the python
+		# that is installed on the system.
 		oldArgs = sys.argv
 		sys.argv = [oldArgs[0], 'py2app', '--semi-standalone']
 
 		# create app
-		##print 'Creating App'
+		#print 'Creating App'
 		try:
 			setup(
 				app=APP,
-				name=self.nameEntry.get(),
+				name=appName,
 				data_files=DATA_FILES,
 				options={'py2app': OPTIONS},
 				setup_requires=['py2app']
 			)
 		except TypeError as e:
-			REMOVETHIS = "later"
+			shutil.rmtree(tempDir)
+			return False
 			#print 'App creation failed: ' + str(e)
 
 		# Clean up temp directory
-		#print 'Cleaning up'
+		# print 'Cleaning up'
 		shutil.rmtree(tempDir)
 		sys.argv = oldArgs # restore the old arguments
+		return True
 
 	def createWidgets(self):
-		self.pack(padx=10, pady=10) # padding for the entire window
+		self.pack(padx=0, pady=0) # padding for the entire window
 		self.configure(background=BG_COLOR)
 
-		labelWidth = 16
-		entryWidth = 34
+		labelWidth = 18
+		entryWidth = 45
+		self.image = PhotoImage(file='logo.ppm', )
+		Label(self, bg=BG_COLOR, foreground=FG_COLOR, image=self.image).pack(fill=BOTH, expand=True, padx=0, pady=0)
+
 		# Source and Destination input
 		pathFrame = Frame(self)
 		pathFrame.configure(background=BG_COLOR)
-		pathFrame.pack(side=TOP, fill=BOTH, expand=True)
+		pathFrame.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=10)
 
-		Label(pathFrame, text='Path to Application:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=0, column=0)
-		Label(pathFrame, text='Path to Launcher:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=1, column=0)
+		Label(pathFrame, text='Path to Application:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=0, column=0)
+		Label(pathFrame, text='Path to Launcher:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=1, column=0)
 
-		self.srcEntry = Entry(pathFrame, highlightbackground=BG_COLOR, width=entryWidth, validate="focusout", validatecommand=self.getinfo)
+		self.srcEntry = Entry(pathFrame, highlightbackground=BG_COLOR, width=entryWidth-5, validate="focusout", validatecommand=self.getinfo)
 		self.srcEntry.grid(row=0, column=1)
-		self.destEntry = Entry(pathFrame, highlightbackground=BG_COLOR, width=entryWidth)
+		self.destEntry = Entry(pathFrame, highlightbackground=BG_COLOR, width=entryWidth-5)
 		self.destEntry.grid(row=1, column=1)
 
-		Button(pathFrame, text='Browse', command=self.openFile, highlightbackground=BG_COLOR, pady=4).grid(row=0, column=2)
-		Button(pathFrame, text='Browse', command=self.openDir, highlightbackground=BG_COLOR, pady=4).grid(row=1, column=2)
+		Button(pathFrame, text='Browse', command=self.openFile, font="Arial 10", highlightbackground=BG_COLOR, pady=5, padx=2).grid(row=0, column=2)
+		Button(pathFrame, text='Browse', command=self.openDir, font="Arial 10", highlightbackground=BG_COLOR, pady=5, padx=2).grid(row=1, column=2)
 
-		self.s0 = ttk.Separator(self, orient=HORIZONTAL)
-		self.s0.pack(side=TOP, fill=BOTH, pady=5)
+		ttk.Separator(self, orient=HORIZONTAL).pack(side=TOP, fill=BOTH, padx=5)
 
 
 
 		# App info preview and editor
 		# Image preview does not work since PIL does not work
-		#	self.iconPreview = Label(self, bg=BG_COLOR).pack()
+		#	self.iconPreview = Label(self, bg=BG_COLOR, foreground=FG_COLOR).pack()
 
 		# Default info plist attributes
 		attributeFrame = Frame(self)
 		attributeFrame.configure(background=BG_COLOR)
-		attributeFrame.pack(side=TOP, fill=BOTH, expand=True)
+		attributeFrame.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=10)
 
 
-		Label(attributeFrame, text='App Name:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=0, column=0)
-		Label(attributeFrame, text='Long Version Number:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=1, column=0)
-		Label(attributeFrame, text='Short Version Number:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=2, column=0)
-		Label(attributeFrame, text='Icon File:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=3, column=0)
+		Label(attributeFrame, text='App Name:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=0, column=0)
+		Label(attributeFrame, text='Long Version Number:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=1, column=0)
+		Label(attributeFrame, text='Short Version Number:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=2, column=0)
+		Label(attributeFrame, text='Icon File:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=3, column=0)
 
 		self.nameEntry = Entry(attributeFrame, highlightbackground=BG_COLOR, width=entryWidth)
 		self.longVersionEntry = Entry(attributeFrame, highlightbackground=BG_COLOR, width=entryWidth)
@@ -179,18 +198,17 @@ class Application(Frame):
 		self.iconEntry.grid(row=3, column=1)
 
 
-		self.s1 = ttk.Separator(self, orient=HORIZONTAL)
-		self.s1.pack(side=TOP, fill=X, pady=5)
+		ttk.Separator(self, orient=HORIZONTAL).pack(side=TOP, fill=X, pady=5, padx=5)
 
 
 		# Options
 		optionsFrame = Frame(self)
 		optionsFrame.configure(background=BG_COLOR)
-		optionsFrame.pack(side=TOP, fill=BOTH, expand=True)
+		optionsFrame.pack(side=TOP, fill=BOTH, expand=True, padx=5, pady=10)
 
-		Label(optionsFrame, text='Shadow Path:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=0, column=0)
-		Label(optionsFrame, text='Preflight:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=1, column=0)
-		Label(optionsFrame, text='Postflight:', bg=BG_COLOR, width=labelWidth, anchor=E).grid(row=2, column=0)
+		Label(optionsFrame, text='Shadow Path:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=0, column=0)
+		Label(optionsFrame, text='Preflight:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=1, column=0)
+		Label(optionsFrame, text='Postflight:', bg=BG_COLOR, foreground=FG_COLOR, width=labelWidth, anchor=E).grid(row=2, column=0)
 
 		self.shadowEntry = Entry(optionsFrame, highlightbackground=BG_COLOR, width=entryWidth)
 		self.preflightEntry = Entry(optionsFrame, highlightbackground=BG_COLOR, width=entryWidth)
@@ -200,7 +218,7 @@ class Application(Frame):
 		self.preflightEntry.grid(row=1, column=1)
 		self.postflightEntry.grid(row=2, column=1)
 
-		Button(self, text="Create", command=self.createApp, width=20, pady=5, highlightbackground=BG_COLOR).pack()
+		Button(self, text="Create", command=self.createApp, width=20, pady=5, highlightbackground=BG_COLOR).pack(pady=10)
 
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
@@ -213,14 +231,14 @@ class Application(Frame):
 		fileoptions['filetypes'] = [('apps', '.app'), ('All Files', '.*')]
 		fileoptions['title'] = 'Open App'
 		self.dir_opt = diroptions = {}
-		diroptions['initialdir'] = '/Volumes/Data/Users/u0823377/'
+		diroptions['initialdir'] = '/Volumes/Data/Users/u/'
 		diroptions['mustexist'] = True
 		diroptions['title'] = 'Save Destination'
 
 		self.createWidgets()
 
 BG_COLOR="lightgrey"
-
+FG_COLOR="black"
 root = Tk()
 root.configure(background=BG_COLOR)
 app=Application(root)
